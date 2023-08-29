a80bfd48ad4b47ff1c3dc3e396cd325db4516c04
Yuriy Volkov <sir.volkov@outlook.com>
2023-08-30 01:07:55 +0300

    sixth commit : task-05

---
+++ js/task-04.js
@@ -0,0 +1,38 @@
+/**
+ * todo :: лічильник складається зі спану і кнопок >>>
+ *  !            --^ які по кліку повинні збільшувати і зменшувати його значення на одиницю
+ *  ?        --> створити змінну counterValue, в якій буде зберігатися поточне значення лічильника
+ *  !            --^ ініціалізувати змінну counterValue значенням 0
+ *  ?        --> додати слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника
+ *  ?        --> оновлюй інтерфейс новим значенням змінної counterValue
+ */
+
+let counterValue = 0;
+
+//-----------------------------------------------------------------
+
+const spanValue = document.querySelector('span#value');
+
+//-----------------------------------------------------------------
+
+const increaseCounterBtn = document.querySelector('[data-action=increment]');
+
+const decreaseCounterBtn = document.querySelector('[data-action=decrement]');
+
+//---------------------------------------------------------------
+
+increaseCounterBtn.addEventListener('click', handleIncreaseCounterBtnClick);
+
+decreaseCounterBtn.addEventListener('click', handleDecreaseCounterBtnClick);
+
+//---------------------------------------------------------------
+
+function handleIncreaseCounterBtnClick() {
+    counterValue += 1;
+    spanValue.textContent = counterValue;
+}
+
+function handleDecreaseCounterBtnClick() {
+    counterValue -= 1;
+    spanValue.textContent = counterValue;
+}
--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,10 @@
+/**
+ * todo :: напиши скрипт, який під час набору тексту в інпуті input#name-input [подія input] >>>
+ *  !            --^ підставляє його поточне значення в span#name-output
+ *  !___ якщо інпут порожній, у спані повинен відображатися рядок "Anonymous"
+
+ */
+
 const formInput = document.querySelector('input#name-input');
 const textOutput = document.querySelector('span#name-output');
 
